79fc2ed0c9ccb4a39a537001b938ac94bd840100
Andrew <sulverus@gmail.com>
2014-08-08 20:40:14 +0400

    Project sceleton updated. command line setting support

---
+++ application/server.py
@@ -0,0 +1,34 @@
+#encoding: utf-8
+
+import tornado.ioloop
+import tornado.web
+import os
+from tornado.options import define, options
+from handlers import MainHandler, PasswordHandler
+
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/password/", PasswordHandler),
+        ]
+        settings = dict(
+            cookie_secret="HUIGMA SECRET KEY",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+def run():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    run()
