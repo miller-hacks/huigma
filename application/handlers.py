174dd8e301e44262ac14ccc0e3b1542641653e47
Andrew <sulverus@gmail.com>
2014-08-08 21:37:39 +0400

    Api handler post request

--- application/handlers.py
+++ application/handlers.py
@@ -14,14 +14,26 @@ class ApiHandler(tornado.web.RequestHandler):
         super(ApiHandler, self).__init__(*args, **kwargs)
         self.set_header('Content-Type', 'application/json; charset="utf-8"')
 
+    def wrong_data(self, message):
+        self.clear()
+        self.set_status(400)
+        self.finish(message)
+
     def get(self, key):
         data = self.application.storage.get(key)
         if data is None:
-            self.clear()
-            self.set_status(400)
-            self.finish('Wrong key')
+            self.wrong_data('Wrong key')
         else:
             self.write(data)
 
     def post(self):
-        self.write("post")
+        content = self.get_argument('content', None)
+        params = dict()
+        for key in ['expire', 'num', 'callback']:
+            value = self.get_argument(key, None)
+            if value:
+                params[key] = value
+        if content:
+            self.write(self.application.storage.save(content, **params))
+        else:
+            self.wrong_data('no content')
